e4daf3ef85f4bc2ba623c6eba58872f6fc2b7303
Alexander <alexanderguest@me.com>
2022-12-29 16:11:23 -0500

    some meaningful commit message

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by Alejandro";
+h2.textContent = "Hey there, friends! Welcome to my very first Webpage.";
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3")
+h3.textContent = "Please enjoy this page while you're here, and remember to..."
+document.querySelector("body").appendChild(h3)
