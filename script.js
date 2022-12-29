2d70de36d1fbe179ee0414140583e6910d3f9b61
Alexander <alexanderguest@me.com>
2022-12-29 17:09:17 -0500

    newnewnew

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ h2.textContent = "Hey there, friends! Welcome to my very first Webpage.";
 document.querySelector("body").appendChild(h2);
 
 const h3 = document.createElement("h3")
-h3.textContent = "Enjoy this page while you're here, and remember..."
+h3.textContent = "Please enjoy this page while you're here, and remember..."
 document.querySelector("body").appendChild(h3)
